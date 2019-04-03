c14a1c344a684b606ca1af3c8695115f8059fd8b
gaoyan <2420926509@qq.com>
2019-04-03 09:43:55 +0800

    初次提交

---
+++ studyproj/src/main/java/cn/myeruka/studyproj/DiscoveryServiceApplication.java
@@ -0,0 +1,17 @@
+package cn.myeruka.studyproj;
+
+/**
+ * Created by Administrator on 2019/1/25.
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServiceApplication.class, args);
+    }
+}
